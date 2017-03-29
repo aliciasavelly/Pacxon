31f6f0a03d5eb2e3aef10a3c1858ff7267e111d8
aliciasavelly <alicia.savelly@gmail.com>
2017-03-29 11:01:57 -0700

    refactor code into multiple files and render pacman characters on grid

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 module.exports = {
-    entry: "./pacxon.js",
-    output: {
-        path: __dirname,
-        filename: "bundle.js"
-    },
-    module: {
-        loaders: [
-            { test: /\.css$/, loader: "style-loader!css-loader" }
-        ]
-    }
+  entry: "./pacxon.js",
+  output: {
+      path: __dirname,
+      filename: "bundle.js"
+  },
+  module: {
+      loaders: [
+          { test: /\.css$/, loader: "style-loader!css-loader" }
+      ]
+  },
+  devtool: 'source-maps'
 };
