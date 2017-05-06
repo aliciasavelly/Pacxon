077784adb7bf49ddc487b64c1c3bf00abe2f003d
aliciasavelly <alicia.savelly@gmail.com>
2017-05-05 17:58:29 -0700

    edit webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   entry: "./pacxon.js",
   output: {
-      path: __dirname,
-      filename: "bundle.js"
+    path: __dirname,
+    filename: "bundle.js"
   },
   module: {
-      loaders: [
-          { test: /\.css$/, loader: "style-loader!css-loader" }
-      ]
+    loaders: [
+      { test: /\.css$/, loader: "style-loader!css-loader" }
+    ]
   },
   devtool: 'source-maps'
 };
