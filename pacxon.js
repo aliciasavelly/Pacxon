fd0489038c5e9a23c05fcefbbaac46738f54e003
aliciasavelly <alicia.savelly@gmail.com>
2017-05-11 16:31:56 -0700

    clean up code

--- pacxon.js
+++ pacxon.js
@@ -1,7 +1,4 @@
 import GameView from './lib/game_view.js';
-// require('./style.css');
-// document.write(require('./lib/game.js'));
-// const GameView = require('./lib/game_view.js');
 
 document.addEventListener("DOMContentLoaded", function(e) {
   let stage = new createjs.Stage("game-canvas");
