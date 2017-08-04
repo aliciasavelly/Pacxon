2a3e140143f59913aa470af812bffe397bb92fb1
aliciasavelly <alicia.savelly@gmail.com>
2017-08-04 14:09:58 -0700

    clean up level class, add switch statement

--- bundle.js
+++ bundle.js
@@ -632,18 +632,24 @@ class Level {
     this.stage = this.game.stage;
     this.ghosts = this.game.ghosts;
 
-    if (level % 6 == 1) {
-      this.setupLevel1();
-    } else if (level % 6 == 2) {
-      this.setupLevel2();
-    } else if (level % 6 == 3) {
-      this.setupLevel3();
-    } else if (level % 6 == 4) {
-      this.setupLevel4();
-    } else if (level % 6 == 5){
-      this.setupLevel5();
-    } else {
-      this.setupLevel6();
+    switch (level % 6) {
+      case 1:
+        this.setupLevel1();
+        break;
+      case 2:
+        this.setupLevel2();
+        break;
+      case 3:
+        this.setupLevel3();
+        break;
+      case 4:
+        this.setupLevel4();
+        break;
+      case 5:
+        this.setupLevel5();
+        break;
+      default:
+        this.setupLevel6();
     }
   }
 
