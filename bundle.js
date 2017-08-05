4785201c4b9eb5d54f9d54d2fc8446a46dbf5138
aliciasavelly <alicia.savelly@gmail.com>
2017-08-04 17:26:04 -0700

    make direction more randomized for ghosts

--- bundle.js
+++ bundle.js
@@ -656,38 +656,38 @@ class Level {
 
   setupLevel1() {
     new Ghost("./lib/assets/pinky_ghost.png", this.stage, this.ghosts, -4, false, .05);
-    new Ghost("./lib/assets/pinky_ghost.png", this.stage, this.ghosts, -4, false, .05);
+    new Ghost("./lib/assets/pinky_ghost.png", this.stage, this.ghosts, 4, false, .05);
   }
 
   setupLevel2() {
     new Ghost("./lib/assets/pinky_ghost.png", this.stage, this.ghosts, 4, false, .05);
-    new Ghost("./lib/assets/pinky_ghost.png", this.stage, this.ghosts, 4, false, .05);
+    new Ghost("./lib/assets/pinky_ghost.png", this.stage, this.ghosts, -4, false, .05);
     new Ghost("./lib/assets/red_ghost.png", this.stage, this.ghosts, 4, true, .05);
   }
 
   setupLevel3() {
     new Ghost("./lib/assets/pinky_ghost.png", this.stage, this.ghosts, 5.5, false, .05);
-    new Ghost("./lib/assets/orange_ghost.png", this.stage, this.ghosts, 4, false, .08);
+    new Ghost("./lib/assets/orange_ghost.png", this.stage, this.ghosts, -4, false, .08);
     new Ghost("./lib/assets/red_ghost.png", this.stage, this.ghosts, 4, true, .05);
   }
 
   setupLevel4() {
     new Ghost("./lib/assets/orange_ghost.png", this.stage, this.ghosts, 4, false, .08);
-    new Ghost("./lib/assets/orange_ghost.png", this.stage, this.ghosts, 4, false, .08);
-    new Ghost("./lib/assets/red_ghost.png", this.stage, this.ghosts, 4, true, .05);
+    new Ghost("./lib/assets/orange_ghost.png", this.stage, this.ghosts, -4, false, .08);
+    new Ghost("./lib/assets/red_ghost.png", this.stage, this.ghosts, -4, true, .05);
   }
 
   setupLevel5() {
     new Ghost("./lib/assets/pinky_ghost.png", this.stage, this.ghosts, 6, false, .05);
-    new Ghost("./lib/assets/orange_ghost.png", this.stage, this.ghosts, 4, false, .08);
+    new Ghost("./lib/assets/orange_ghost.png", this.stage, this.ghosts, -4, false, .08);
     new Ghost("./lib/assets/red_ghost.png", this.stage, this.ghosts, 4, true, .05);
     new Ghost("./lib/assets/red_ghost.png", this.stage, this.ghosts, 4, true, .05);
   }
 
   setupLevel6() {
     new Ghost("./lib/assets/pinky_ghost.png", this.stage, this.ghosts, 5, false, .05);
-    new Ghost("./lib/assets/orange_ghost.png", this.stage, this.ghosts, 4, false, .08);
-    new Ghost("./lib/assets/orange_ghost.png", this.stage, this.ghosts, 4, false, .08);
+    new Ghost("./lib/assets/orange_ghost.png", this.stage, this.ghosts, -4, false, .08);
+    new Ghost("./lib/assets/orange_ghost.png", this.stage, this.ghosts, -4, false, .08);
     new Ghost("./lib/assets/orange_ghost.png", this.stage, this.ghosts, 4, false, .08);
   }
 }
